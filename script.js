d1e8d2bf2917efb129e252a750bae707206c5b22
anastasiavitebskaia1985-a11y <anastasiavitebskaia1985@gmail.com>
2025-10-10 01:19:54 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.querySelectorAll('[data-cta]').forEach(btn=>{
-  btn.addEventListener('click',()=>{
-    if(window.gtag){ gtag('event','cta_click',{label:btn.dataset.label||'cta'}); }
-  });
-});
